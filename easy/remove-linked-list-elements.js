b790f1387cd0577e505cf9454d75ec539830e9c4
Cong <1012296791@qq.com>
2022-09-09 21:44:52 +0800

    。

--- easy/remove-linked-list-elements.js
+++ easy/remove-linked-list-elements.js
@@ -1,13 +1,26 @@
 /**
+ * 递归
+ * 通过递归进入到最内侧链表
+ * 然后用return来逐层处理当前层val是否相等
+ */
+var removeElements = function (head, val) {
+  if (!head) {
+    return head
+  }
+  head.next = removeElements(head.next, val);
+  return head.val == val ? head.next : head;
+};
+
+/**
  * 迭代
  * 在head外再套一层链表防止出现
  *  需要删除第一个链表元素
  *  所有链表都有删除
- * 当链表的next不为null时就循环 
- * 判 断next的v al是否等于val
- * 最后返回newhead的next，原链表项
- */
-var removeElements = function (head, val) {
+ * 当链表的next不为null时就循环
+ * 判断next的val是否等于val
+    后返回newhead的next，原链表项
+  /
+  r removeElements = function (head, val) {
   let newhead = {
     val: 0,
     next: head
